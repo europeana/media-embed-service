4270c37b182c82c51df92630dbc463a7068d9837
andyjmaclean <andyjmaclean@gmail.com>
2020-04-17 21:13:52 +0200

    EA-2028 Added basic e2e - test tweak

--- tests/spec-e2e/width-and-height-parameters.test.js
+++ tests/spec-e2e/width-and-height-parameters.test.js
@@ -4,35 +4,30 @@ const manifest_url = `http%3A%2F%2F127.0.0.1%3A${base_port}%2Ftests%2Ffixture-da
 const target_url = base_url + '?manifest=' + manifest_url;
 
 const waitTime = 10000;
-const selTime = '.canvas-time';
-const selPlay = '.button-play';
-const selPlayIcon = selPlay + ' .av-icon';
-
 const selCanvas = '.canvas-container';
-
 
 const fmtWHParam = (w, h) => {
   return `&width=${w}&height=${h}`
 };
 
 const confirmSizeFromParams = (browser, w, h) => {
-  return browser.url(target_url + fmtWHParam(w, h))
-  .waitForElementVisible(selCanvas, waitTime)
+  browser.url(target_url + fmtWHParam(w, h))
+  .waitForElementPresent(selCanvas, waitTime)
   .getElementSize(selCanvas, function (result) {
     this.assert.ok(result.value.width === w, `Check width (${result.value.width}) is ${w}`);
     this.assert.ok(result.value.height === h, `Check height (${result.value.height}) is ${h}`);
   })
+  .end();
 };
-
 
 module.exports = {
   'Limit the player size according to the url parameters (500 / 400)': (browser) => {
-    confirmSizeFromParams(browser, 500, 400).end()
+    confirmSizeFromParams(browser, 500, 400);
   },
   'Limit the player size according to the url parameters (800 / 500)': (browser) => {
-    confirmSizeFromParams(browser, 800, 500).end()
+    confirmSizeFromParams(browser, 800, 500);
   },
   'Limit the player size according to the url parameters (1200 / 800)': (browser) => {
-    confirmSizeFromParams(browser, 800, 500).end()
+    confirmSizeFromParams(browser, 800, 500);
   }
 };
